14f7d122a97928f154867855233abdd4a2277d37
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-05-16 03:28:50 +0530

    feat(kafka): Added kafka writer

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,19 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/VinukaThejana/go-utils/logger"
 	"github.com/flitlabs/spotoncars-stream-go/internal/app/pkg/controllers"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/lib"
+	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/services"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
@@ -46,7 +51,7 @@ func main() {
 		AllowedHeaders: []string{"Content-Type", "X-CSRF-Token"},
 	}))
 
-	router.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+	router.Get("/view", func(w http.ResponseWriter, _ *http.Request) {
 		w.Header().Set("Content-Type", "text/event-stream")
 		w.Header().Set("Cache-Control", "no-cache")
 		w.Header().Set("Connection", "Keep-alive")
@@ -54,12 +59,101 @@ func main() {
 		streamC.Subscribe(w, e.KafkaTopic, kafka.LastOffset)
 	})
 
-	router.Get("/logs", func(w http.ResponseWriter, _ *http.Request) {
+	router.Get("/view/{topic}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/event-stream")
 		w.Header().Set("Cache-Control", "no-cache")
 		w.Header().Set("Connection", "Keep-alive")
 
-		streamC.Subscribe(w, "log", kafka.FirstOffset)
+		offset := kafka.LastOffset
+
+		topic := chi.URLParam(r, "topic")
+		switch topic {
+		case "":
+			topic = e.KafkaTopic
+		case "log":
+			offset = kafka.LastOffset
+		case "logs":
+			topic = "log"
+			offset = kafka.FirstOffset
+		default:
+		}
+
+		streamC.Subscribe(w, topic, offset)
+	})
+
+	router.Post("/add/{topic}", func(w http.ResponseWriter, r *http.Request) {
+		const maxRequestBodySize = 1 << 20
+		w.Header().Set("Content-Type", "application/json")
+		type Response map[string]interface{}
+
+		contentType := r.Header.Get("Content-Type")
+		if !strings.HasPrefix(contentType, "application/json") {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			json.NewEncoder(w).Encode(Response{
+				"status":  "bad_request",
+				"message": "only content of type application/json can be sent",
+			})
+			go services.Log(&connector, &e, fmt.Sprintf("Sending invalid content type : %v", contentType))
+			return
+		}
+
+		r.Body = http.MaxBytesReader(w, r.Body, maxRequestBodySize)
+		defer r.Body.Close()
+
+		topic := chi.URLParam(r, "topic")
+		if topic == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(Response{
+				"status":  "bad_request",
+				"message": "topic is not provided",
+			})
+			go services.Log(&connector, &e, "topic is not provided")
+			return
+		}
+
+		var data map[string]interface{}
+
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				w.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(w).Encode(Response{
+					"status":  "bad_request",
+					"message": "body cannot be empty, please provide valid json",
+				})
+				go services.Log(&connector, &e, "the provided request body is empty")
+				return
+			}
+
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(Response{
+				"status":  "bad_request",
+				"message": "failed to parse data invalid json",
+			})
+			go services.Log(&connector, &e, "invalid json object provided by the client")
+			return
+		}
+
+		dataBytes, err := json.Marshal(data)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(Response{
+				"status":  "internal_server_error",
+				"message": "something went wrong on the server side",
+			})
+			go services.Log(&connector, &e, fmt.Sprintf("Error when marshaling the requst body\n%v", err))
+			return
+		}
+		dataStr := string(dataBytes)
+
+		connector.KafkaWriteToTopic(&e, topic, dataStr)
+		go services.Log(&connector, &e, fmt.Sprintf("Logged %v", dataStr))
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(Response{
+			"status":  "okay",
+			"message": "added the message to the topic",
+		})
 	})
 
 	router.Get("/health", func(w http.ResponseWriter, _ *http.Request) {
