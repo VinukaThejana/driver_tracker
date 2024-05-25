92097b2be23134900ecfa5fecab506db6d20e47e
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-05-25 10:10:47 +0530

    refactor(quality): refactored the code to reduce clutter

--- cmd/main.go
+++ cmd/main.go
@@ -2,203 +2,121 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
+	"time"
 
-	"github.com/flitlabs/spotoncars-stream-go/internal/app/pkg/controllers"
+	"github.com/flitlabs/spotoncars-stream-go/internal/app/pkg/routes"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
-	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/lib"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"github.com/segmentio/kafka-go"
-	"go.uber.org/zap"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var (
 	e         env.Env
 	connector connections.C
-	log       *zap.SugaredLogger
 
-	streamC controllers.Stream
+	addR    routes.Route
+	viewR   routes.Route
+	healthR routes.Route
 )
 
 func init() {
 	e.Load()
 	connector.InitRedis(&e)
 
-	logger, err := zap.NewProduction()
-	lib.LogFatal(err)
-	defer logger.Sync()
-	log = logger.Sugar()
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out: os.Stderr,
+	})
 
-	streamC = controllers.Stream{
+	addR = &routes.Add{
 		E: &e,
 		C: &connector,
-		L: log,
+	}
+	viewR = &routes.View{
+		E: &e,
+		C: &connector,
+	}
+	healthR = &routes.Health{
+		E: &e,
+		C: &connector,
 	}
 }
 
-func main() {
-	errCh := make(chan error, 1)
+func router() *chi.Mux {
+	r := chi.NewRouter()
 
-	router := chi.NewRouter()
-
-	router.Use(middleware.RequestID)
-	router.Use(middleware.RealIP)
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	router.Use(cors.Handler(cors.Options{
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"https://*", "http://*"},
 		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
 		AllowedHeaders: []string{"Content-Type", "X-CSRF-Token"},
 	}))
 
-	router.Get("/view", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "Keep-alive")
+	r.MethodFunc(viewR.Method(), viewR.Path(), viewR.Handler)
+	r.MethodFunc(addR.Method(), addR.Path(), addR.Handler)
+	r.MethodFunc(healthR.Method(), healthR.Path(), healthR.Handler)
 
-		streamC.Subscribe(w, e.KafkaTopic, kafka.LastOffset)
-	})
+	return r
+}
 
-	router.Get("/view/{topic}", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "Keep-alive")
+func shutdown(ctx context.Context, server *http.Server) {
+	shutdownCtx, shutdownCancel := context.WithTimeout(ctx, 5*time.Second)
+	defer shutdownCancel()
 
-		offset := kafka.LastOffset
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Error().Err(err).Msg("failed to shudown server gracefully")
+		return
+	}
 
-		topic := chi.URLParam(r, "topic")
-		switch topic {
-		case "":
-			topic = e.KafkaTopic
-		case "log":
-			offset = kafka.LastOffset
-		case "logs":
-			topic = "log"
-			offset = kafka.FirstOffset
-		default:
-		}
+	log.Info().Msg("server shutdown gracefully")
+}
 
-		streamC.Subscribe(w, topic, offset)
-	})
-
-	router.Post("/add/{topic}", func(w http.ResponseWriter, r *http.Request) {
-		const maxRequestBodySize = 1 << 20
-		w.Header().Set("Content-Type", "application/json")
-		type Response map[string]interface{}
-
-		contentType := r.Header.Get("Content-Type")
-		if !strings.HasPrefix(contentType, "application/json") {
-			w.WriteHeader(http.StatusUnsupportedMediaType)
-			json.NewEncoder(w).Encode(Response{
-				"status":  "bad_request",
-				"message": "only content of type application/json can be sent",
-			})
-			log.Error("invalid content type provided", zap.String("Content-Type", contentType))
-			return
-		}
-
-		r.Body = http.MaxBytesReader(w, r.Body, maxRequestBodySize)
-		defer r.Body.Close()
-
-		topic := chi.URLParam(r, "topic")
-		if topic == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(Response{
-				"status":  "bad_request",
-				"message": "topic is not provided",
-			})
-			log.Error("topic is not provided")
-			return
-		}
-
-		var data map[string]interface{}
-
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				w.WriteHeader(http.StatusBadRequest)
-				json.NewEncoder(w).Encode(Response{
-					"status":  "bad_request",
-					"message": "body cannot be empty, please provide valid json",
-				})
-				log.Error("request body is empty")
-				return
-			}
-
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(Response{
-				"status":  "bad_request",
-				"message": "failed to parse data invalid json",
-			})
-			log.Error("invalid json object provided by the client")
-			return
-		}
-
-		dataBytes, err := json.Marshal(data)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(Response{
-				"status":  "internal_server_error",
-				"message": "something went wrong on the server side",
-			})
-			log.Error("error when marshalling the requst body", zap.Error(err))
-			return
-		}
-		dataStr := string(dataBytes)
-
-		connector.KafkaWriteToTopic(&e, topic, dataStr)
-
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(Response{
-			"status":  "okay",
-			"message": "added the message to the topic",
-		})
-	})
-
-	router.Get("/health", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-
-		html := `
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Health Check</title>
-    </head>
-    <body>
-        <h1>Everything is working as expected</h1>
-    </body>
-</html>
-    `
-
-		w.Write([]byte(html))
-	})
+func main() {
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", e.Port),
+		Handler: router(),
+	}
 
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
 
-	log.Infow("Started the HTTP server", zap.Int("address", e.Port))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log.Info().Msgf("started the HTTP server and listening on port : %d", e.Port)
 	go func() {
-		if err := http.ListenAndServe(fmt.Sprintf(":%d", e.Port), router); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Error("HTTP server error", zap.Error(err))
-			errCh <- err
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Error().Err(err).Msg("HTTP server error")
+				return
+			}
 		}
 	}()
 
 	select {
 	case sig := <-signalCh:
-		log.Info("shutting down server", zap.String("cause", "signal"), zap.String("signal", sig.String()))
-	case err := <-errCh:
-		log.Error("shutting down server", zap.String("cause", "error"), zap.Error(err))
+		log.Info().Str("cause", "signal").Str("signal", sig.String()).Msg("shutting down server")
+		shutdown(ctx, server)
+		cancel()
+	case <-ctx.Done():
+		log.Info().Msg("context cancelled, shutting down the server")
+		shutdown(ctx, server)
+		cancel()
 	}
 }
