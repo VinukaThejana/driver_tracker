ee05fa08e10fb65c9a5a49777483e89d3b4a4e24
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-05-26 23:46:59 +0530

    fix(route): used the new json handler

--- internal/app/pkg/routes/add.go
+++ internal/app/pkg/routes/add.go
@@ -32,17 +32,11 @@ func (add *Add) Path() string {
 // Handler is the bussiness logic of the Add route
 func (add *Add) Handler(w http.ResponseWriter, r *http.Request) {
 	const maxRequestBodySize = 1 << 20
-	w.Header().Set("Content-Type", "application/json")
-	type Response map[string]interface{}
 
 	contentType := r.Header.Get("Content-Type")
 	if !strings.HasPrefix(contentType, "application/json") {
 		log.Error().Str("Content-Type", contentType).Msg("invalid content type provided")
-		w.WriteHeader(http.StatusUnsupportedMediaType)
-		json.NewEncoder(w).Encode(Response{
-			"status":  "bad_request",
-			"message": "only content of type application/json can be sent",
-		})
+		sendJSONResponse(w, http.StatusBadRequest, "only content of type application/json can be sent")
 		return
 	}
 
@@ -52,11 +46,7 @@ func (add *Add) Handler(w http.ResponseWriter, r *http.Request) {
 	topic := chi.URLParam(r, "topic")
 	if topic == "" {
 		log.Error().Msg("topic is not provided")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(Response{
-			"status":  "bad_request",
-			"message": "topic is not provided",
-		})
+		sendJSONResponse(w, http.StatusBadRequest, "topic is not provided")
 		return
 	}
 
@@ -66,40 +56,24 @@ func (add *Add) Handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if errors.Is(err, io.EOF) {
 			log.Error().Msg("request body is empty")
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(Response{
-				"status":  "bad_request",
-				"message": "body cannot be empty, please provide valid json",
-			})
+			sendJSONResponse(w, http.StatusBadRequest, "body cannot be empty, please provide valid json")
 			return
 		}
 
 		log.Error().Msg("invalid request body provided by the client")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(Response{
-			"status":  "bad_request",
-			"message": "failed to parse data invalid json",
-		})
+		sendJSONResponse(w, http.StatusBadRequest, "failed to parse data invalid json")
 		return
 	}
 
 	dataBytes, err := json.Marshal(data)
 	if err != nil {
 		log.Error().Err(err).Msg("error marshaling the request body")
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(Response{
-			"status":  "internal_server_error",
-			"message": "something went wrong on the server side",
-		})
+		sendJSONResponse(w, http.StatusInternalServerError, "something went wrong on the server side")
 		return
 	}
 	dataStr := string(dataBytes)
 
 	add.C.KafkaWriteToTopicWithHTTP(add.E, topic, dataStr)
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(Response{
-		"status":  "okay",
-		"message": "added the message to the topic",
-	})
+	sendJSONResponse(w, http.StatusOK, "added the message to the topic")
 }
