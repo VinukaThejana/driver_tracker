f90c6495d1f6af962e0f215ee83fd4737583e548
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-05-26 23:43:33 +0530

    feat(helpers): added route helpers

--- internal/app/pkg/routes/routes.go
+++ internal/app/pkg/routes/routes.go
@@ -1,11 +1,30 @@
 // Package routes contains all the routes
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type response map[string]interface{}
 
 // Route interface consits methods that should be implmented by a specific route
 type Route interface {
 	Method() string
 	Path() string
 	Handler(http.ResponseWriter, *http.Request)
+}
+
+func sendJSONResponse(w http.ResponseWriter, statusCode int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response{
+		"message": message,
+	})
+}
+
+func sendJSONResponseWInterface(w http.ResponseWriter, statusCode int, res interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(res)
 }
