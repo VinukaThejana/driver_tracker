f7b46895302bc46461ed03d68f7994b6322d6cc1
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-05-29 23:53:37 +0530

    fix(websocket): fix the websocket not auto closing issue

--- internal/app/pkg/websockets/view.go
+++ internal/app/pkg/websockets/view.go
@@ -1,13 +1,10 @@
 package websockets
 
 import (
-	"context"
 	"net/http"
-	"time"
 
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
-	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/lib"
 	"github.com/go-chi/chi/v5"
 	"github.com/lesismal/nbio/nbhttp/websocket"
 	"github.com/rs/zerolog/log"
@@ -32,17 +29,11 @@ func (view *View) Path() string {
 
 // Handler is the bussiness logic of the view websocket
 func (view *View) Handler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3600)
-	defer cancel()
-
 	topic := chi.URLParam(r, "topic")
 	if topic == "" {
 		log.Error().Msg("topic is not provided")
 		return
 	}
-
-	reader := view.C.KafkaReader(view.E, topic, kafka.LastOffset)
-	defer reader.Close()
 
 	upgrader := websocket.NewUpgrader()
 	upgrader.CheckOrigin = func(r *http.Request) bool {
@@ -50,32 +41,44 @@ func (view *View) Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	upgrader.OnOpen(func(c *websocket.Conn) {
 		log.Info().Str("addr", c.RemoteAddr().String()).Msg("connection opened")
-		for {
-			message, _ := reader.ReadMessage(ctx)
-			log.Error().Interface("message", message).Msg("The message is given")
-			payload, err := lib.ToStr(string(message.Value))
+
+		done := make(chan struct{})
+
+		go func() {
+			reader := view.C.KafkaReader(view.E, topic, kafka.LastOffset)
+			defer reader.Close()
+			defer close(done)
+
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					message, _ := reader.ReadMessage(r.Context())
+					if len(message.Value) == 0 {
+						continue
+					}
+					if err := c.WriteMessage(websocket.TextMessage, message.Value); err != nil {
+						log.Error().Err(err).Msg("error sending data to the websocket client")
+						return
+					}
+				}
+			}
+		}()
+
+		c.OnClose(func(c *websocket.Conn, err error) {
 			if err != nil {
-				log.Error().Err(err).Str("value", string(message.Value)).Msg("Error occured when serialization and deserialization")
-				return
+				log.Error().Err(err).Str("addr", c.RemoteAddr().String()).Msg("connection closed with error")
+			} else {
+				log.Info().Str("addr", c.RemoteAddr().String()).Msg("connection closed")
 			}
-
-			if err := c.WriteMessage(websocket.TextMessage, payload); err != nil {
-				log.Error().Err(err).Msg("error sending data to the websocket client")
-				break
-			}
-		}
-
-		c.Close()
+			done <- struct{}{}
+		})
 	})
 
-	conn, err := upgrader.Upgrade(w, r, nil)
+	_, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Error().Err(err).Msg("error occured while upgrading the websocket connection")
 		return
 	}
-
-	conn.OnClose(func(c *websocket.Conn, err error) {
-		log.Info().Str("addr", c.RemoteAddr().String()).Err(err).Msg("connection closed")
-		cancel()
-	})
 }
