c279424b007d984c45c992dff305ab077541d23c
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-11 15:48:38 +0530

    feat(added-close): added the ability to close all connections upon server close

--- internal/pkg/connections/connections.go
+++ internal/pkg/connections/connections.go
@@ -5,4 +5,15 @@ package connections
 type C struct {
 	// R contains all Redis related databases
 	R *Redis
+	// K contains all Kafka writers
+	K *KafkaWriters
+}
+
+// Close is a function that is used to close all the connections
+func (c *C) Close() {
+	// close all the kafka writers
+	c.K.B.Close()
+
+	// close all the redis clients
+	c.R.DB.Close()
 }
