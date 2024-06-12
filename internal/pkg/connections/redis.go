7cee6724c13411fbdae1251a5dfd29e147049947
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-13 00:50:10 +0530

    feat(redis): added the ability to lock and unlock a key in Redis

--- internal/pkg/connections/redis.go
+++ internal/pkg/connections/redis.go
@@ -2,6 +2,7 @@ package connections
 
 import (
 	"context"
+	"time"
 
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/lib"
@@ -11,6 +12,32 @@ import (
 // Redis contains all Redis connections
 type Redis struct {
 	DB *redis.Client
+}
+
+// AcquireLock is a function that is used to lock redis keys until an operation is finished modifiying it
+func (r *Redis) AcquireLock(ctx context.Context, client *redis.Client, lockKey, lockValue string, lockDuration, waitDuration time.Duration) (bool, error) {
+	for {
+		acquired, err := client.SetNX(ctx, lockKey, lockValue, lockDuration).Result()
+		if err != nil {
+			return false, err
+		}
+
+		if acquired {
+			return true, nil
+		}
+
+		select {
+		case <-ctx.Done():
+			return false, ctx.Err()
+		default:
+			time.Sleep(waitDuration)
+		}
+	}
+}
+
+// ReleaseLock is a function that is used to release the added lock
+func (r *Redis) ReleaseLock(client *redis.Client, lockKey string) {
+	client.Del(context.Background(), lockKey)
 }
 
 // InitRedis is a function that is used to intialize redis databases
