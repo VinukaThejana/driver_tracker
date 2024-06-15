9a0c01d9293699675d888b9b21cd423f229b7c04
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-15 13:11:41 +0530

    feat(add-middleware): added the middleware to validate the booking token

--- internal/app/pkg/middlewares/auth.go
+++ internal/app/pkg/middlewares/auth.go
@@ -2,14 +2,32 @@ package middlewares
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/flitlabs/spotoncars-stream-go/internal/app/pkg/tokens"
+	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
+	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
 )
 
-// DriverContext contains the driver context
-type DriverContext string
+type (
+	// BookingTokenIDCtx contains the type for the booking token ID
+	BookingTokenIDCtx string
+	// DriverIDCtx contians the type for the driver ID
+	DriverIDCtx string
+	// BookingIDCtx contains the type for the booking ID type
+	BookingIDCtx string
+	// PartitionNoCtx contains the type for the partition no type
+	PartitionNoCtx string
+)
 
-// DriverContextKey is the drvier context key that is used to identify the driver in the context
-const DriverContextKey DriverContext = "driver_id"
+const (
+	BookingTokenID BookingTokenIDCtx = "booking_token_id"
+	DriverID       DriverIDCtx       = "driver_id"
+	BookingID      BookingIDCtx      = "booking_id"
+	PartitionNo    PartitionNoCtx    = "partition_no"
+)
 
 // IsDriver is a middleware that is used to check wether the driver is logged in
 func IsDriver(next http.Handler) http.Handler {
@@ -24,7 +42,58 @@ func IsDriver(next http.Handler) http.Handler {
 
 		driverID := 2
 
-		ctx := context.WithValue(r.Context(), DriverContextKey, driverID)
+		ctx := context.WithValue(r.Context(), DriverID, driverID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// IsBookingTokenValid is a middleware that is used to check wether the booking token is valid or not
+func IsBookingTokenValid(next http.Handler, c *connections.C, e *env.Env) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bookingToken := ""
+		unauthorizedErr := fmt.Errorf("you are not authorized to perform this operation")
+
+		authorization := strings.Split(r.Header.Get("Authorization"), " ")
+		if len(authorization) == 2 {
+			bookingToken = authorization[1]
+		} else {
+			bookingTokenC, err := r.Cookie("booking_token")
+			if err != nil {
+				http.Error(w, unauthorizedErr.Error(), http.StatusUnauthorized)
+				return
+			}
+
+			bookingToken = bookingTokenC.Value
+		}
+
+		if bookingToken == "" {
+			http.Error(w, unauthorizedErr.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		bt := tokens.BookingToken{
+			C: c,
+			E: e,
+		}
+		isValid, token := bt.Validate(r.Context(), bookingToken)
+		if !isValid {
+			http.Error(w, unauthorizedErr.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		id, driverID, bookingID, partitionNo, err := bt.Get(token)
+		if err != nil {
+			http.Error(w, unauthorizedErr.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		ctx := r.Context()
+
+		ctx = context.WithValue(ctx, BookingTokenID, id)
+		ctx = context.WithValue(ctx, DriverID, driverID)
+		ctx = context.WithValue(ctx, BookingID, bookingID)
+		ctx = context.WithValue(ctx, PartitionNo, partitionNo)
+
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
