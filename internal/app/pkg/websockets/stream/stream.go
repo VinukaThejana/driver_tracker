019eff4196037e92207e91cc7585a21de06a1949
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-15 16:36:24 +0530

    feat(protected): protected the needed rotues with middlewares

--- internal/app/pkg/websockets/stream/stream.go
+++ internal/app/pkg/websockets/stream/stream.go
@@ -4,6 +4,7 @@ package stream
 import (
 	"net/http"
 
+	"github.com/flitlabs/spotoncars-stream-go/internal/app/pkg/middlewares"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
 	"github.com/go-chi/chi/v5"
@@ -12,12 +13,17 @@ import (
 // WebSocket contains all the websockets that are related to the stream
 func WebSocket(e *env.Env, c *connections.C) http.Handler {
 	r := chi.NewRouter()
-	r.Get("/view/{topic}", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/view/{booking_id}", func(w http.ResponseWriter, r *http.Request) {
 		view(w, r, e, c)
 	})
 
-	r.Get("/add/{topic}", func(w http.ResponseWriter, r *http.Request) {
-		add(w, r, e, c)
+	r.Route("/add", func(r chi.Router) {
+		r.Use(func(h http.Handler) http.Handler {
+			return middlewares.IsBookingTokenValid(h, e, c)
+		})
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			add(w, r, e, c)
+		})
 	})
 
 	return r
