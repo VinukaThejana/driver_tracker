4432452940d5a146dfbd622ad537b74807c76baf
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-15 13:58:22 +0530

    feat(added-middlewares): added middlewares and optimized the route layouts

--- internal/app/pkg/routes/stream/stream.go
+++ internal/app/pkg/routes/stream/stream.go
@@ -16,15 +16,25 @@ func Router(e *env.Env, c *connections.C) http.Handler {
 	r.Get("/view", func(w http.ResponseWriter, r *http.Request) {
 		view(w, r, e, c)
 	})
-	r.Route("/", func(r chi.Router) {
+	r.Route("/create", func(r chi.Router) {
 		r.Use(middlewares.IsContentJSON)
-		r.Post("/create", func(w http.ResponseWriter, r *http.Request) {
+		r.Use(middlewares.IsDriver)
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 			create(w, r, e, c)
 		})
-		r.Post("/add/{topic}", func(w http.ResponseWriter, r *http.Request) {
-			add(w, r, e, c)
+	})
+
+	r.Route("/", func(r chi.Router) {
+		r.Use(func(h http.Handler) http.Handler {
+			return middlewares.IsBookingTokenValid(h, c, e)
 		})
-		r.Post("/end", func(w http.ResponseWriter, r *http.Request) {
+		r.Route("/add", func(r chi.Router) {
+			r.Use(middlewares.IsContentJSON)
+			r.Post("/{topic}", func(w http.ResponseWriter, r *http.Request) {
+				add(w, r, e, c)
+			})
+		})
+		r.Delete("/end", func(w http.ResponseWriter, r *http.Request) {
 			end(w, r, e, c)
 		})
 	})
