4b198c4298f4f6231f7a7ebc662a5ca233805d5b
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-16 16:41:25 +0530

    fix(refactored): renamed the booking token logic containing file

---
+++ internal/app/pkg/routes/bookings/bookings.go
@@ -0,0 +1,21 @@
+// Package bookings contains routes that are related to bookings
+package bookings
+
+import (
+	"net/http"
+
+	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
+	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
+	"github.com/go-chi/chi/v5"
+)
+
+// Router contains the bookings router
+func Router(e *env.Env, c *connections.C) http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/view/{booking_id}", func(w http.ResponseWriter, r *http.Request) {
+		view(w, r, e, c)
+	})
+
+	return r
+}
