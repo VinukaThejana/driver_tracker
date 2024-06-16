413268686fad611fd0a4af2aa4c21ec938dca1a9
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-16 17:22:26 +0530

    feat(remove-env): removed the kafka topic env variable

--- internal/pkg/env/env.go
+++ internal/pkg/env/env.go
@@ -20,7 +20,6 @@ type Env struct {
 	KafkaUsername       string        `mapstructure:"KAFKA_USERNAME" validate:"required"`
 	KafkaPassword       string        `mapstructure:"KAFKA_PASSWORD" validate:"required"`
 	KafkaBroker         string        `mapstructure:"KAFKA_BROKER" validate:"required"`
-	KafkaTopic          string        `mapstructure:"KAFKA_TOPIC" validate:"required"`
 	KafkaRestURL        string        `mapstructure:"KAFKA_REST_URL" validate:"required,url"`
 	RedisDBURL          string        `mapstructure:"REDIS_DB_URL" validate:"required"`
 	Domain              string        `mapstructure:"DOMAIN" validate:"required,url"`
