32013479db5ca4dd831d268c3b91a074c58eb2d9
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-19 00:10:56 +0530

    feat(get-location): get the starting location and ending location latitude and longitude cordinates in the response json

--- internal/app/pkg/routes/bookings/view.go
+++ internal/app/pkg/routes/bookings/view.go
@@ -1,23 +1,33 @@
 package bookings
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/lib"
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog/log"
+	"googlemaps.github.io/maps"
 )
 
 type bookingDetails struct {
-	BookRefNo    *string
-	DriverName   *string
-	ContactNo    *string
-	VehicleModal *string
-	VehicleRegNo *string
+	BookRefNo      *string
+	DriverName     *string
+	ContactNo      *string
+	VehicleModal   *string
+	VehicleRegNo   *string
+	BookPickUpAddr *string
+	BookDropAddr   *string
+}
+
+type geo struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
 }
 
 // view is a route that is used to get the booking information by the booking id
@@ -33,12 +43,11 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	if val != "" {
 		started = true
 	}
-	fmt.Printf("started: %v\n", started)
 
 	var payload bookingDetails
 
-	query := "SELECT BookRefNo, DriverName, ContactNo, VehicleModal, VehicleRegNo FROM Tbl_BookingDetails WHERE BookRefNo = @BookRefNo"
-	err := c.DB.QueryRow(query, sql.Named("BookRefNo", bookingID)).Scan(&payload.BookRefNo, &payload.DriverName, &payload.ContactNo, &payload.VehicleModal, &payload.VehicleRegNo)
+	query := "SELECT BookRefNo, DriverName, ContactNo, VehicleModal, VehicleRegNo, BookPickUpAddr, BookDropAddr FROM Tbl_BookingDetails WHERE BookRefNo = @BookRefNo"
+	err := c.DB.QueryRow(query, sql.Named("BookRefNo", bookingID)).Scan(&payload.BookRefNo, &payload.DriverName, &payload.ContactNo, &payload.VehicleModal, &payload.VehicleRegNo, &payload.BookPickUpAddr, &payload.BookDropAddr)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to get the query from the database")
 		lib.JSONResponse(w, http.StatusInternalServerError, "something went wrong, please try again later")
@@ -49,6 +58,10 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	ContactNo := ""
 	VehicleModal := ""
 	VehicleRegNo := ""
+
+	pickups := []geo{}
+	dropoffs := []geo{}
+
 	if payload.DriverName != nil {
 		DriverName = *payload.DriverName
 	}
@@ -61,16 +74,71 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	if payload.VehicleRegNo != nil {
 		VehicleRegNo = *payload.VehicleRegNo
 	}
+	if payload.BookPickUpAddr != nil {
+		payload, err := geocode(r.Context(), c, seperator(*payload.BookPickUpAddr))
+		if err != nil {
+			log.Error().Err(err).Msg("failed to geocode the pickup address")
+			lib.JSONResponse(w, http.StatusInternalServerError, "something went wrong, please try again later")
+			return
+		}
+		pickups = append(pickups, payload...)
+	}
+	if payload.BookDropAddr != nil {
+		payload, err := geocode(r.Context(), c, seperator(*payload.BookDropAddr))
+		if err != nil {
+			log.Error().Err(err).Msg("failed to geocode the drop address")
+			lib.JSONResponse(w, http.StatusInternalServerError, "something went wrong, please try again later")
+			return
+		}
 
-	data := map[string]interface{}{}
+		dropoffs = append(dropoffs, payload...)
+	}
+
+	fmt.Printf("pickups: %v\n", pickups)
+	fmt.Printf("dropoffs: %v\n", dropoffs)
+
+	data := map[string]any{}
 	data["active"] = started
 	data["driver_name"] = DriverName
 	data["contact_no"] = ContactNo
 	data["vehicle_modal"] = VehicleModal
 	data["vehicle_registration_no"] = VehicleRegNo
+	data["pickups"] = pickups
+	data["dropoffs"] = dropoffs
 	if started {
 		data["stream"] = fmt.Sprintf("ws://%s/ws/stream/view/%s", e.Host, bookingID)
 	}
 
 	lib.JSONResponseWInterface(w, http.StatusOK, data)
+}
+
+func seperator(str string) []string {
+	if strings.Contains(str, "|") {
+		return strings.Split(str, "|")
+	}
+
+	return []string{str}
+}
+
+func geocode(ctx context.Context, c *connections.C, data []string) ([]geo, error) {
+	pickups := []geo{}
+
+	for _, pickup := range data {
+		route, err := c.M.Geocode(ctx, &maps.GeocodingRequest{
+			Address: pickup,
+		})
+		if err != nil {
+			return []geo{}, nil
+		}
+		if len(route) == 0 {
+			return []geo{}, fmt.Errorf("failed to geocode the street address")
+		}
+
+		pickups = append(pickups, geo{
+			Lat: route[0].Geometry.Location.Lat,
+			Lon: route[0].Geometry.Location.Lng,
+		})
+	}
+
+	return pickups, nil
 }
