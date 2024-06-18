ba6c60075ed25e2bf42eca7ae804f3bd178d276a
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-19 00:14:28 +0530

    feat(add-package): added a package to handle useful errors

---
+++ internal/pkg/errors/errors.go
@@ -0,0 +1,12 @@
+// Package errors contains essential errors
+package errors
+
+import "fmt"
+
+var (
+	ErrServer       = fmt.Errorf("something went wrong, please try again later")
+	ErrBusy         = fmt.Errorf("server is busy right now, please try again later")
+	ErrUnauthorized = fmt.Errorf("you are not authorized to perform this operation")
+	ErrBadRequest   = fmt.Errorf("invalid data, please check and try agan")
+	ErrNotAdmin     = fmt.Errorf("you are not an admin to perform this operation")
+)
