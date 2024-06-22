33293339fe5dbf3aa395283c857f8c7d54192249
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-22 14:46:13 +0530

    feat(add-script): added a script to udpate the google cloud run account env variables

--- justfile
+++ justfile
@@ -6,3 +6,6 @@ default:
 # Connect with the Redis database
 redis:
   iredis --url $(echo $REDIS_DB_URL)
+
+prd_env:
+  bash ./scripts/prd_secrets.sh $DOPPLER_PRD_SERVICE_ACCOUNT $SERVICE_NAME $SERVICE_REGION
---
+++ scripts/prd_secrets.sh
@@ -0,0 +1,42 @@
+#Get the production secrets from Doppler and update the cloud run account environmental varables
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Please provide the Doppler PRD service account secret to update the env variables"
+	exit 1
+fi
+if [ -z "$2" ]; then
+	echo "Please provide the Google run service name"
+	exit 1
+fi
+if [ -z "$3" ]; then
+	echo "Please provide the Google run service region"
+	exit 1
+fi
+
+secret=$1
+service_name=$2
+service_regioin=$3
+
+env=$(
+	curl --request GET \
+		--url 'https://api.doppler.com/v3/configs/config/secrets/download?format=env' \
+		--header "Authorization: Bearer $secret"
+)
+
+result=""
+
+while IFS= read -r line; do
+	key=$(echo "$line" | cut -d'=' -f1)
+	value=$(echo "$line" | cut -d'=' -f2 | tr -d '"')
+
+	if [ -n "$key" ]; then
+		if [ -z "$result" ]; then
+			result="${key}=${value}"
+		else
+			result="${result},${key}=${value}"
+		fi
+	fi
+done <<<"$env"
+
+gcloud run services update $service_name --update-env-vars $result --region $service_regioin
