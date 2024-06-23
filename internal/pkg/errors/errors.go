03995222f389a183051b1368f001e895226dcf00
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-23 22:41:58 +0530

    chore(refactor): added comments to errors

--- internal/pkg/errors/errors.go
+++ internal/pkg/errors/errors.go
@@ -4,10 +4,16 @@ package errors
 import "fmt"
 
 var (
-	ErrServer            = fmt.Errorf("something went wrong, please try again later")
-	ErrBusy              = fmt.Errorf("server is busy right now, please try again later")
-	ErrUnauthorized      = fmt.Errorf("you are not authorized to perform this operation")
-	ErrBadRequest        = fmt.Errorf("invalid data, please check and try agan")
-	ErrNotAdmin          = fmt.Errorf("you are not an admin to perform this operation")
+	// ErrServer is to indicate an internal server error
+	ErrServer = fmt.Errorf("something went wrong, please try again later")
+	// ErrBusy is to indicate that the server is busy at the moment
+	ErrBusy = fmt.Errorf("server is busy right now, please try again later")
+	// ErrUnauthorized is to indicate the user is unauthorized to perform the given operation
+	ErrUnauthorized = fmt.Errorf("you are not authorized to perform this operation")
+	// ErrBadRequest is to indicate that the request is a bad request
+	ErrBadRequest = fmt.Errorf("invalid data, please check and try agan")
+	// ErrNotAdmin is to indicate that the requesting user is not the admin
+	ErrNotAdmin = fmt.Errorf("you are not an admin to perform this operation")
+	// ErrBookingIDNotValid is to indicate that the given booking id is not valid
 	ErrBookingIDNotValid = fmt.Errorf("booking id you provided is not valid")
 )
