8528a543861f0c5f18dcf67a9a1267479679bd87
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-24 00:23:49 +0530

    fix(service): use a service

--- internal/app/pkg/routes/stream/end.go
+++ internal/app/pkg/routes/stream/end.go
@@ -1,13 +1,13 @@
 package stream
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/bytedance/sonic"
 	"github.com/flitlabs/spotoncars_stream/internal/app/pkg/middlewares"
+	"github.com/flitlabs/spotoncars_stream/internal/app/pkg/services"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/env"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/errors"
@@ -28,8 +28,6 @@ func end(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) {
 		log.Warn().Interface("payload", payload)
 		return
 	}
-
-	partition := payload[0]
 
 	pipe := c.R.DB.Pipeline()
 
@@ -54,53 +52,5 @@ func end(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) {
 	})
 	lib.JSONResponse(w, http.StatusOK, "ended the session")
 
-	go func() {
-		ctx := context.Background()
-
-		startOffset := int64(payload[1])
-		endOffset, err := c.GetLastOffset(ctx, e, e.Topic, partition)
-
-		if startOffset >= endOffset {
-			log.Warn().Msg("no messages in the given partition")
-			return
-		}
-
-		if err != nil {
-			log.Error().Err(err).Msg("failed to get the last offset")
-			log.Warn().Interface("payload", payload)
-			return
-		}
-		defer func() {
-			log.Warn().Int("start", int(startOffset)).Int("end", int(endOffset))
-		}()
-
-		messages, err := c.GetLastNMessages(ctx, e, startOffset, endOffset, e.Topic, partition)
-		if err != nil {
-			log.Error().Err(err).Msg("failed to get the last messages")
-			return
-		}
-		payload, err := sonic.MarshalString(messages)
-		if err != nil {
-			log.Error().Err(err).Msg("failed to marshal the messages")
-			return
-		}
-
-		err = c.InitStorage(e)
-		if err != nil {
-			log.Error().Err(err).Int("start", int(startOffset)).Int("end", int(endOffset)).Msg("failed to initialize the storage client")
-			return
-		}
-		defer c.S.Close()
-
-		bucket := c.S.Bucket(e.BucketName)
-		object := bucket.Object(bookingID)
-
-		w := object.NewWriter(ctx)
-		defer w.Close()
-		_, err = fmt.Fprint(w, payload)
-		if err != nil {
-			log.Error().Err(err).Msg("failed to write the messages to the google cloud storage")
-			return
-		}
-	}()
+	go services.SaveBooking(e, c, payload, bookingID)
 }
