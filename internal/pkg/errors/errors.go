da39733f0d533398e91097c5e2446fcf9d419a90
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-23 13:44:00 +0530

    feat(add-error): added the booking id not valid error

--- internal/pkg/errors/errors.go
+++ internal/pkg/errors/errors.go
@@ -4,9 +4,10 @@ package errors
 import "fmt"
 
 var (
-	ErrServer       = fmt.Errorf("something went wrong, please try again later")
-	ErrBusy         = fmt.Errorf("server is busy right now, please try again later")
-	ErrUnauthorized = fmt.Errorf("you are not authorized to perform this operation")
-	ErrBadRequest   = fmt.Errorf("invalid data, please check and try agan")
-	ErrNotAdmin     = fmt.Errorf("you are not an admin to perform this operation")
+	ErrServer            = fmt.Errorf("something went wrong, please try again later")
+	ErrBusy              = fmt.Errorf("server is busy right now, please try again later")
+	ErrUnauthorized      = fmt.Errorf("you are not authorized to perform this operation")
+	ErrBadRequest        = fmt.Errorf("invalid data, please check and try agan")
+	ErrNotAdmin          = fmt.Errorf("you are not an admin to perform this operation")
+	ErrBookingIDNotValid = fmt.Errorf("booking id you provided is not valid")
 )
