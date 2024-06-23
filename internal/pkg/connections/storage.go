e4c30dd092aaccb451263f42254327e8a6f03941
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-23 20:56:39 +0530

    fix(test): testing some stuff

--- internal/pkg/connections/storage.go
+++ internal/pkg/connections/storage.go
@@ -3,6 +3,7 @@ package connections
 import (
 	"context"
 	"encoding/base64"
+	"fmt"
 
 	"cloud.google.com/go/storage"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
@@ -11,6 +12,7 @@ import (
 
 // InitStorage is a function that is used to initialize the Google cloud storage
 func (c *C) InitStorage(e *env.Env) error {
+	fmt.Println(e.GcloudAPIKey)
 	key, err := base64.StdEncoding.DecodeString(e.GcloudAPIKey)
 	if err != nil {
 		return err
