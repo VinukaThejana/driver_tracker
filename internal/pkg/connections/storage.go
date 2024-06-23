729899a97140c8124ce815bebf1754e7a6e5c78d
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-23 22:25:52 +0530

    feat(decoder): experimenting with a new deocoder

--- internal/pkg/connections/storage.go
+++ internal/pkg/connections/storage.go
@@ -2,27 +2,21 @@ package connections
 
 import (
 	"context"
-	"encoding/base64"
-	"strings"
+	"fmt"
 
 	"cloud.google.com/go/storage"
 	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/env"
+	"github.com/flitlabs/spotoncars-stream-go/internal/pkg/lib"
 	"google.golang.org/api/option"
 )
 
 // InitStorage is a function that is used to initialize the Google cloud storage
 func (c *C) InitStorage(e *env.Env) error {
-	var gcloudAPIKey string
-	if strings.HasSuffix(e.GcloudAPIKey, "==") {
-		gcloudAPIKey = e.GcloudAPIKey
-	} else {
-		gcloudAPIKey = e.GcloudAPIKey + "=="
-	}
-
-	key, err := base64.StdEncoding.DecodeString(gcloudAPIKey)
+	key, err := lib.Base64URLDecode(e.GcloudAPIKey)
 	if err != nil {
 		return err
 	}
+	fmt.Println(string(key))
 
 	client, err := storage.NewClient(context.Background(), option.WithCredentialsJSON(key))
 	if err != nil {
