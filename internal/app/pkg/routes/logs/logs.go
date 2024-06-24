5b543fa6d7f74e3b43d17f0823848dd2037c15fb
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-24 16:56:32 +0530

    feat(add-middleware): added a middleware to authenticate the admin

--- internal/app/pkg/routes/logs/logs.go
+++ internal/app/pkg/routes/logs/logs.go
@@ -4,6 +4,7 @@ package logs
 import (
 	"net/http"
 
+	"github.com/flitlabs/spotoncars_stream/internal/app/pkg/middlewares"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/env"
 	"github.com/go-chi/chi/v5"
@@ -13,11 +14,16 @@ import (
 func Router(e *env.Env, c *connections.C) http.Handler {
 	r := chi.NewRouter()
 
-	r.Get("/view/{booking_id}", func(w http.ResponseWriter, r *http.Request) {
-		view(w, r, e, c)
-	})
-	r.Delete("/delete/{booking_id}", func(w http.ResponseWriter, r *http.Request) {
-		delete(w, r, e, c)
+	r.Route("/", func(r chi.Router) {
+		r.Use(func(h http.Handler) http.Handler {
+			return middlewares.IsAdmin(h, e, c)
+		})
+		r.Get("/view/{booking_id}", func(w http.ResponseWriter, r *http.Request) {
+			view(w, r, e, c)
+		})
+		r.Delete("/delete/{booking_id}", func(w http.ResponseWriter, r *http.Request) {
+			delete(w, r, e, c)
+		})
 	})
 
 	return r
