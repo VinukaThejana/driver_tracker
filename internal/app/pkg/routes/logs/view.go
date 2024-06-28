78196f3095e341bfe7d91c77646d6f17c326dc4a
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-06-28 21:57:06 +0530

    fix(err): send the booking log even if the booking id cannot be found in the database

--- internal/app/pkg/routes/logs/view.go
+++ internal/app/pkg/routes/logs/view.go
@@ -2,13 +2,14 @@ package logs
 
 import (
 	"database/sql"
+	"errors"
 	"io"
 	"net/http"
 
 	"github.com/bytedance/sonic"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/connections"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/env"
-	"github.com/flitlabs/spotoncars_stream/internal/pkg/errors"
+	errs "github.com/flitlabs/spotoncars_stream/internal/pkg/errors"
 	"github.com/flitlabs/spotoncars_stream/internal/pkg/lib"
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog/log"
@@ -23,13 +24,13 @@ type bookingDetails struct {
 func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) {
 	bookingID := chi.URLParam(r, "booking_id")
 	if bookingID == "" {
-		lib.JSONResponse(w, http.StatusBadRequest, errors.ErrBookingIDNotValid.Error())
+		lib.JSONResponse(w, http.StatusBadRequest, errs.ErrBookingIDNotValid.Error())
 	}
 
 	err := c.InitStorage(e)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to initialize the storage client")
-		lib.JSONResponse(w, http.StatusInternalServerError, errors.ErrServer.Error())
+		lib.JSONResponse(w, http.StatusInternalServerError, errs.ErrServer.Error())
 		return
 	}
 	defer c.S.Close()
@@ -40,7 +41,7 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	reader, err := object.NewReader(r.Context())
 	if err != nil {
 		log.Error().Err(err).Str("booking_id", bookingID).Msg("failed to initialize the reader, either because the booking id is not valid or some autentication error")
-		lib.JSONResponse(w, http.StatusBadRequest, errors.ErrBookingIDNotValid.Error())
+		lib.JSONResponse(w, http.StatusBadRequest, errs.ErrBookingIDNotValid.Error())
 		return
 	}
 	defer reader.Close()
@@ -48,11 +49,11 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	data, err := io.ReadAll(reader)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to read the data")
-		lib.JSONResponse(w, http.StatusInternalServerError, errors.ErrServer.Error())
+		lib.JSONResponse(w, http.StatusInternalServerError, errs.ErrServer.Error())
 		return
 	}
 	if data == nil {
-		lib.JSONResponse(w, http.StatusBadRequest, errors.ErrBookingIDNotValid.Error())
+		lib.JSONResponse(w, http.StatusBadRequest, errs.ErrBookingIDNotValid.Error())
 		return
 	}
 
@@ -60,7 +61,7 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	err = sonic.Unmarshal(data, &payload)
 	if err != nil {
 		log.Error().Err(err).Interface("payload", payload).Msg("failed to marshal the data from the payload")
-		lib.JSONResponse(w, http.StatusInternalServerError, errors.ErrServer.Error())
+		lib.JSONResponse(w, http.StatusInternalServerError, errs.ErrServer.Error())
 		return
 	}
 
@@ -68,9 +69,11 @@ func view(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) 
 	query := "SELECT DriverName, VehicleModal, VehicleRegNo FROM Tbl_BookingDetails WHERE BookRefNo = @BookRefNo"
 	err = c.DB.QueryRow(query, sql.Named("BookRefNo", bookingID)).Scan(&bookingDetails.DriverName, &bookingDetails.VehicleModal, &bookingDetails.VehicleRegNo)
 	if err != nil {
-		log.Error().Err(err).Msg("failed to get the query from the database")
-		lib.JSONResponse(w, http.StatusInternalServerError, errors.ErrServer.Error())
-		return
+		if !errors.Is(err, sql.ErrNoRows) {
+			log.Error().Err(err).Msg("failed to get the query from the database")
+			lib.JSONResponse(w, http.StatusInternalServerError, errs.ErrServer.Error())
+			return
+		}
 	}
 	DriverName := ""
 	VehicleModal := ""
