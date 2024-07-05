febbb42d527a4b966fba032164912817adbab553
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-07-05 06:17:03 +0530

    feat(route): ability to end job passing the given timeframe

---
+++ internal/app/pkg/routes/jobs/check_job.go
@@ -0,0 +1,100 @@
+package jobs
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/bytedance/sonic"
+	"github.com/flitlabs/spotoncars_stream/internal/app/pkg/services"
+	"github.com/flitlabs/spotoncars_stream/internal/pkg/connections"
+	"github.com/flitlabs/spotoncars_stream/internal/pkg/env"
+	"github.com/rs/zerolog/log"
+)
+
+func checkJob(w http.ResponseWriter, r *http.Request, e *env.Env, c *connections.C) {
+	var wg sync.WaitGroup
+
+	client := c.R.DB
+	jobs := client.SMembers(r.Context(), e.PartitionManagerKey).Val()
+	wg.Add(len(jobs))
+
+	for _, job := range jobs {
+		go func(job string) {
+			defer wg.Done()
+
+			val := client.Get(r.Context(), job).Val()
+			if val != "" {
+				return
+			}
+
+			val = client.Get(r.Context(), "n"+job).Val()
+			if val == "" {
+				log.Warn().
+					Str("job", job).
+					Str("redis-value", val).
+					Msg("the job cannot be found with n- partitions")
+				return
+			}
+
+			var payload []string
+			err := sonic.UnmarshalString(val, &payload)
+			if err != nil {
+				log.Error().
+					Err(err).
+					Str("job", job).
+					Str("redis-value", val).
+					Msg("cannot unmarshal the payload")
+				return
+			}
+			if len(payload) != 2 {
+				log.Error().
+					Err(
+						fmt.
+							Errorf("the payload did not contains the booking ID and the offset")).
+					Str("job", job).
+					Str("redis-value", val).
+					Interface("payload", payload).
+					Msg("the payload in n- partition is not equal to two")
+				return
+			}
+			startOffset, err := strconv.Atoi(payload[1])
+			if err != nil {
+				log.Error().
+					Err(err).
+					Str("job", job).
+					Str("redis-value", val).
+					Msg("failed to convert the offset to int")
+				return
+			}
+			partition, err := strconv.Atoi(job)
+			if err != nil {
+				log.Error().
+					Err(err).
+					Str("job", job).
+					Str("redis-value", val).
+					Msg("failed to convert the job to integer")
+				return
+			}
+
+			pipe := client.Pipeline()
+
+			pipe.SRem(r.Context(), e.PartitionManagerKey, job)
+			pipe.Del(r.Context(), "n"+job)
+
+			_, err = pipe.Exec(r.Context())
+			if err != nil {
+				log.Error().
+					Err(err).
+					Str("job", job).
+					Str("redis-value", val).
+					Msg("failed to delete the key in redis")
+			}
+
+			go services.SaveBooking(e, c, []int{partition, startOffset}, payload[0])
+		}(job)
+	}
+
+	fmt.Fprint(w, "okay")
+}
